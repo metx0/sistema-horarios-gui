ecbd4fd32aea4ee92b01c850cf5754d2b14256a1
Miguel Alejandro <migtorruco@gmail.com>
2024-09-12 15:06:24 -0600

    initial commit: basic structure

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+# Used for text alignment
+from PyQt5.QtCore import Qt
+
+app = QApplication(sys.argv)
+
+# Load the QSS (Qt Style Sheet) file
+with open("styles.qss", "r") as f:
+    app.setStyleSheet(f.read())
+
+# Create the main window
+window = QWidget()
+window.setWindowTitle("Sistema escolar de horarios")
+
+# Create the title
+title = QLabel("Sistema escolar para generar horarios", window)
+
+# Give it an "id"
+title.setObjectName("title")
+
+# Center it horizontally
+title.setAlignment(Qt.AlignHCenter)
+
+# Create a VBox layout to place the title
+vbox = QVBoxLayout()
+vbox.addWidget(title)
+window.setLayout(vbox)
+
+window.showMaximized()
+
+sys.exit(app.exec_())
