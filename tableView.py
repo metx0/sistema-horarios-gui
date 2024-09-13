6ea972ada6d5ccf79ee86b3ef9c95fbbb0fae4f0
Valeria-Lee <leev6893a@gmail.com>
2024-09-12 23:34:19 -0600

    subject table structure

--- tableView.py
+++ tableView.py
@@ -1,39 +1,56 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QWidget, QTableWidget, QTableWidgetItem
 import sqlite3
-import sys
 
-# TODO: mejorar las tablas para que se puedan usar con otra info.
+class TeacherTable(QTableWidget):
+    def __init__(self):
+        super().__init__()
 
-con = sqlite3.connect("db_horario.db")
-cur = con.cursor()
+        self.con = sqlite3.connect("db_horario.db")
+        self.cur = self.con.cursor()
 
-app = QApplication(sys.argv)
+        self.make_queries()
+        self.make_table()
 
-resId = cur.execute("SELECT id_maestro from maestro")
-idMaestro = resId.fetchall()
-resName = cur.execute("SELECT nombre from maestro")
-nombre = resName.fetchall()
-resTipo = cur.execute("SELECT tipo_maestro from maestro")
-tipoMaestro = resTipo.fetchall()
-# Retorna tuplas, entonces, es necesario convertir a listas para acceder a valores
-teachers_data = [list(idMaestro), list(nombre), list(tipoMaestro)]
+    def make_queries(self):
+        self.resId = self.cur.execute("SELECT id_maestro from maestro")
+        self.idMaestro = self.resId.fetchall()
+        self.resName = self.cur.execute("SELECT nombre from maestro")
+        self.nombre = self.resName.fetchall()
+        self.resTipo = self.cur.execute("SELECT tipo_maestro from maestro")
+        self.tipoMaestro = self.resTipo.fetchall()
+        # Retorna tuplas, entonces, es necesario convertir a listas para acceder a valores
+        self.teachers_data = [list(self.idMaestro), list(self.nombre), list(self.tipoMaestro)]
 
-table = QTableWidget()
+    def make_table(self):
+        self.setRowCount(len(self.idMaestro))
+        self.setColumnCount(len(self.teachers_data))
+        self.setHorizontalHeaderLabels(["idMaestro", "nombre", "tipoMaestro"])
+        self.fill_table()
 
-# print(teachers_data)
+    def fill_table(self):
+        for i in range(len(self.idMaestro)):
+            item_idMaestro = QTableWidgetItem(str(self.idMaestro[i][0]))
+            item_nombre = QTableWidgetItem(self.nombre[i][0])
+            item_tipoMaestro = QTableWidgetItem(str(self.tipoMaestro[i][0]))
+            self.setItem(i, 0, item_idMaestro)
+            self.setItem(i, 1, item_nombre)
+            self.setItem(i, 2, item_tipoMaestro)
 
-table.setRowCount(len(idMaestro))
-table.setColumnCount(len(teachers_data))
-table.setHorizontalHeaderLabels(["idMaestro", "nombre", "tipoMaestro"])
+class SubjectTable(QTableWidget):
+    def __init__(self):
+        super().__init__()
 
-for i in range(len(teachers_data)):
-    item_idMaestro = QTableWidgetItem(idMaestro[i][0])
-    item_nombre = QTableWidgetItem(nombre[i][0])
-    item_tipoMaestro = QTableWidgetItem(tipoMaestro[i][0])
-    # Add items to the table.
-    table.setItem(i, 0, item_idMaestro)
-    table.setItem(i, 1, item_nombre)
-    table.setItem(i, 2, item_tipoMaestro)
+        self.con = sqlite3.connect("db_horario.db")
+        self.cur = self.con.cursor()
 
-table.show()
-sys.exit(app.exec())
+        self.make_queries()
+        self.make_table()
+
+    def make_queries(self):
+        pass
+
+    def make_table(self):
+        pass
+
+    def fill_table(self):
+        pass
